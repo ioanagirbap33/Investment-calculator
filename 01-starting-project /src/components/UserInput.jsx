cd7211cea3b9b16cb90ee52df1a084652873d8c6
ioana.girba@principal33.com <ioana.girba@192-168-0-144.rdsnet.ro>
2024-03-10 22:09:53 +0200

    adding inputs and labels

---
+++ 01-starting-project /src/components/UserInput.jsx
@@ -0,0 +1,24 @@
+export const UserInput = () => {
+  return (
+    <section id="section">
+      <div className="input-group">
+        <p>
+          <label>Initial Investment</label>
+          <input type="number" required />
+        </p>
+        <p>
+          <label>Anual Investment</label>
+          <input type="number" required />
+        </p>
+        <p>
+          <label>Expected Return</label>
+          <input type="number" required />
+        </p>
+        <p>
+          <label>Duration</label>
+          <input type="number" required />
+        </p>
+      </div>
+    </section>
+  );
+};
